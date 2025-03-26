0ffa02a881898f46fd1b08d320bd351d4fa1dbed
adhikariprashamsa586@gmail.com <adhikariprashamsa586@gmail.com>
2025-03-26 23:22:46 +0545

    superkey

---
+++ superKey.java
@@ -0,0 +1,30 @@
+class baseClass {
+    int value = 10;
+
+    baseClass(String msg) {
+        System.out.println("Message from Baseclass" + msg);
+    }
+
+    void display() {
+        System.out.println("Displayfunction from baseClass");
+
+    }
+}
+
+class ChildClass
+extend baseClass
+{
+
+    ChildClass() {
+        super("Calling from ChildClass");
+        System.out.println("Value from Baseclass:" + super.value);
+        super.display();
+    }
+}
+
+public class superKey {
+    public static void main(String[] args) {
+        ChildClass c1 = new ChildClass();
+    }
+
+}
